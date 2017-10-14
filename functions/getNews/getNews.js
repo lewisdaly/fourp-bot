a92a817588a2358be541c0cf78fe41cd61f514b6
Lewis Daly <lewis.daly@enabled.com.au>
2017-10-14 18:37:57 +1030

    Working on loading news dynamically by language

--- functions/getNews/getNews.js
+++ functions/getNews/getNews.js
@@ -1,9 +1,92 @@
 
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 const paths = require('../common/definitions').paths;
 
-module.exports = functions.https.onRequest((req, res) => {
-  console.log('hey', paths);
 
-  return res.status(200).send(true);
+/**
+ * query params:
+ * - language ISO 63903 *required*, must be eng, tgl, ceb
+ */
+module.exports = functions.https.onRequest((req, res) => {
+  const { language } = req.query;
+
+  if (!language) {
+    return res.status(400).send('language is required. must be one of \'eng, tgl, ceb\'');
+  }
+
+  if (['eng', 'tgl', 'ceb'].indexOf('ceb') == -1) {
+    return res.status(400).send('language must be one of \'eng, tgl, ceb\'');
+  }
+
+  return getNews(language)
+  .then(news => {
+
+    return res.status(200).send(news);
+  })
+  .catch(err => {
+    console.log(err);
+    return res.status(500).send(err);
+  });
 });
+
+
+/**
+ *
+ */
+const formatMessage = (stories) => {
+  // return {
+  //   messages: stories
+  // };
+
+
+  return {
+   "messages": [
+      {
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"list",
+            "top_element_style":"large",
+            "elements":[
+              {
+                "title":"Chatfuel Rockets T-Shirt",
+                "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
+              },
+              {
+                "title":"Chatfuel Rockets Hoodie",
+                "subtitle":"Soft gray cotton t-shirt with CF Rockets logo",
+              }
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
+
+/**
+ * Format a message for chatfuel
+ * ref: http://docs.chatfuel.com/plugins/plugin-documentation/json-api
+ */
+const formatStory = (title, content) => {
+  return {text: content};
+}
+
+const getNews = (language) => {
+  return admin.database().ref(paths.news).once('value')
+  .then(snapshot => snapshot.val())
+  .then(values => {
+    console.log('values', Object.keys(values).filter(key => key.indexOf(language) > -1));
+
+    //Filter for the language code:
+    const stories = Object.keys(values)
+                 .filter(key => key.indexOf(language) > -1)
+                 .map(key => values[key])
+                 .map(value => formatStory('1', value));
+
+    return formatMessage(stories);
+  });
+}
---
+++ functions/readme.md
@@ -0,0 +1,11 @@
+
+
+## Testing locally:
+```
+nvm use v6.11.1
+
+
+firebase serve --only functions
+
+curl "http://localhost:5000/fourp-bot/us-central1/getNews?language=ceb"
+```
