303589b292039e5df32448a8be74b568d94e1b16
Lewis Daly <lewis.daly@enabled.com.au>
2017-10-24 20:28:53 +1030

    Minor script updates

--- scripts/setup_get_started.sh
+++ scripts/setup_get_started.sh
@@ -10,5 +10,27 @@ curl -X GET "$URL"
 URL="https://graph.facebook.com/v2.6/me/messenger_profile?access_token=$PAGE_ACCESS_TOKEN"
 curl -X POST -H "Content-Type: application/json" -d '{
   "get_started": {"payload":"getStarted"},
+	"persistent_menu": [{
+		"locale":"default",
+    "composer_input_disabled": true,
+    "call_to_actions":[
+      {
+        "title":"Get Started",
+        "type":"postback",
+        "payload":"Hi"
+      },
+      {
+        "title":"Menu",
+        "type":"postback",
+        "payload":"menu"
+      },
+      {
+        "type":"web_url",
+        "title":"4PBot Site",
+        "url":"https://4pbot.com",
+        "webview_height_ratio":"full"
+      }
+    ]
+	}],
   "greeting":[{"locale":"default", "text":"Hello! I am 4PBot!"}]
 }' "$URL"
