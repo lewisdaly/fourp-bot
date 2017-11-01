ab70741c7f2414eded93689277397e40d85a6188
Lewis Daly <lewis.daly@enabled.com.au>
2017-11-01 15:43:42 +1030

    Fixing .env variables

--- functions/common/env.js
+++ functions/common/env.js
@@ -1,7 +1,17 @@
 const env = require('node-env-file');
 
 module.exports = (functions) => {
-	console.log('node_env:', process.env.NODE_ENV);
+
+	let envFilename = null;
+	switch (process.env.NODE_ENV) {
+		case 'local':
+		case 'production':
+			envFilename = `.env_${process.env.NODE_ENV}`;
+			break;
+		default:
+			console.warn('WARNING: NODE_ENV not set. defaulting to production');
+			envFilename = '.env_production';
+	}
 
   let verify_token = null;
   let page_access_token = null;
@@ -9,26 +19,14 @@ module.exports = (functions) => {
   let studio_command_uri = null;
   let firebase_uri = null;
 
-  //TODO: not ideal. Don't infer, Lewis.
-  // if (!functions.config().verify_token) {
-  //   console.log("not running in firebase");
-    env(__dirname + '/../common/.env_common');
+  env(__dirname + '/.env_common');
+	env(__dirname + '/' + envFilename);
 
-    verify_token = process.env.verify_token;
-    page_access_token = process.env.page_access_token;
-    studio_token = process.env.studio_token;
-    studio_command_uri = process.env.studio_command_uri;
-    firebase_uri = process.env.firebase_uri;
-  // } else {
-  //   console.log("running in firebase");
-  //
-  //   verify_token = functions.config().fn_bot.verify_token;
-  //   access_token = functions.config().fn_bot.access_token;
-  //   studio_token = functions.config().fn_bot.studio_token;
-  //   studio_command_uri = functions.config().fn_bot.studio_command_uri;
-  //   firebase_uri = functions.config().fn_bot.firebase_uri;
-  //   // firebase_uri = "https://fourp-bot.firebaseio.com";
-  // }
+  verify_token = process.env.verify_token;
+  page_access_token = process.env.page_access_token;
+  studio_token = process.env.studio_token;
+  studio_command_uri = process.env.studio_command_uri;
+  firebase_uri = process.env.firebase_uri;
 
   return {
     verify_token,
--- scripts/setup_get_started.sh
+++ scripts/setup_get_started.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/../functions/common/.env_production
+
+if [ -z "$1" ]; then
+	echo 'No environment set. Defaulting to production'.
+	source $DIR/../functions/common/.env_production
+else
+	source $DIR/../functions/common/.env_"$1"
+fi
 
 URL="https://graph.facebook.com/v2.6/me/messenger_profile?fields=get_started,greeting&access_token=$PAGE_ACCESS_TOKEN"
 curl -X GET "$URL"
